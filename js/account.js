2e56fda50bf09d051d0da2b04519fbf771f90c54
kekaadrenalin <kekapor@outlook.com>
2017-04-14 16:54:37 +0300

    new

--- js/account.js
+++ js/account.js
@@ -1,8 +1,27 @@
 (function($){
-    // переключение tab-ов для how-block
-    // $('#how-block--tabs a').click(function(e){
+    // переключение tab-ов для menu
+    // $('#menu a').click(function(e){
         // e.preventDefault();
         // $(this).tab('show');
     // });
+    
+    // yandex-map
+    var myMap;
+    ymaps.ready(initYaMap);
+    
+    function initYaMap() { 
+        myMap = new ymaps.Map('map-in-left-menu', {
+            center: [55.76, 37.64],
+            zoom: 7,
+            controls: [],
+        });
+        
+        myPlacemark = new ymaps.Placemark([55.76, 37.64], { 
+            hintContent: '1', 
+            balloonContent: 'Столица России' 
+        });
+
+        myMap.geoObjects.add(myPlacemark);
+    }
     
 })(jQuery);
