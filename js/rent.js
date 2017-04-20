6c750de5727a35eb08d249f7ad5c69b647a14f61
kekaadrenalin <kekapor@outlook.com>
2017-04-20 14:23:18 +0300

    new pages

--- js/rent.js
+++ js/rent.js
@@ -1,4 +1,39 @@
 (function($){
-    // 
+    // слайдер фото
+    $('.slider-main').slick({
+        lazyLoad: 'progressive',
+        slide: '.slide-item',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.slider-main',
+        arrows: false,
+        focusOnSelect: true,
+        variableWidth: true,
+    });
+    
+    // yandex-map
+    var myMap;
+    ymaps.ready(initYaMap);
+    
+    function initYaMap() { 
+        myMap = new ymaps.Map('map-in-rent-offer', {
+            center: [55.76, 37.64],
+            zoom: 16,
+            controls: [],
+        });
+        
+        myPlacemark = new ymaps.Placemark([55.76, 37.64], { 
+            hintContent: '1', 
+            balloonContent: 'Столица России' 
+        });
+
+        myMap.geoObjects.add(myPlacemark);
+    }
     
 })(jQuery);
